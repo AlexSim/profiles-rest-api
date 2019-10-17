0af56f0c1481522063e02b0e8ede79e3601697e4
Alex Sim <h.c.sim@bham.ac.uk>
2019-10-17 10:54:30 +0100

    Added POST, PUT, PATCH, DELETE to our ViewSet

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from profiles_api import views
 
 
+"""need to map for viewset and url"""
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view(), name=''),
+    path('', include(router.urls)),
 ]
