266651acfc3c6604818d3df200da73adb15a0352
Alex Sim <h.c.sim@bham.ac.uk>
2019-10-19 09:30:29 +0100

    Added login

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -97,10 +99,18 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updting profiles"""
     serializer_class = serializers.UserProfileSrializer
 
+    # HTTP Method
     queryset = models.UserProfile.objects.all()
     
+    # Check permoission
     authentication_classes = (TokenAuthentication, )
     permission_classes = (permissions.UpdateOwnProfile,)
     
+    # Search 
     filter_backends = (filters.SearchFilter, )
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
